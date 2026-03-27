bcc2e5a3b52d76778bdc6298ed7c33bc49c0308d
MuyesserAylin <savuranmuyesseraylin@gmail.com>
2026-03-27 20:06:48 +0300

    Initial commit

---
+++ backend/offer-system/src/main/java/com/simulation/offer_system/OfferSystemApplication.java
@@ -0,0 +1,13 @@
+package com.simulation.offer_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfferSystemApplication.class, args);
+	}
+
+}
---
+++ backend/offer-system/src/test/java/com/simulation/offer_system/OfferSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simulation.offer_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfferSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
